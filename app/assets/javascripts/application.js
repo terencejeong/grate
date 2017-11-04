47d0629d147baa8c6213fe475eab7f2fc29b63cd
Terence Jeong <terence.jeong@gmail.com>
2017-11-04 18:00:54 +1100

    javascript changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,12 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 //= require turbolinks
-//= require_tree .
-
+//= require jquery3
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
