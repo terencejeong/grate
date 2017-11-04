0e58ec4d7e8784ddc4799ac0e92c9a6db5d7a534
Terence Jeong <terence.jeong@gmail.com>
2017-11-04 21:04:02 +1100

    can comment on an item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,18 @@ class ItemsController < ApplicationController
   # GET /items.json
   def index
     @items = Item.all
+    session[:conversations] ||= []
+
+  @users = User.all.where.not(id: current_user)
+  @conversations = Conversation.includes(:recipient, :messages)
+                               .find(session[:conversations])
   end
 
   # GET /items/1
   # GET /items/1.json
   def show
+    @user = User.find(params[:id])
+    @conversations = Conversation.includes(:recipient, :messages)
   end
 
   # GET /items/new
