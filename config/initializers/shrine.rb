3e51ca8d2acc26c873f8f5321cb301242adf5ab5
Terence Jeong <terence.jeong@gmail.com>
2017-11-08 11:55:38 +1100

    added cloudinary

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,11 +1,14 @@
-require "shrine"
-require "shrine/storage/file_system"
+require "cloudinary"
+require "shrine/storage/cloudinary"
+
+
+Cloudinary.config(
+  cloud_name: "deerfwkm9",
+  api_key:ENV["CLOUDINARY_API"],
+  api_secret:ENV["CLOUDINARY_SECRET"]
+)
 
 Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"), # permanent
+  cache: Shrine::Storage::Cloudinary.new(prefix: "cache"), # for direct uploads
+  store: Shrine::Storage::Cloudinary.new(prefix: "store"),
 }
-
-Shrine.plugin :activerecord
-Shrine.plugin :cached_attachment_data # for forms
-Shrine.plugin :rack_file # for non-Rails apps
